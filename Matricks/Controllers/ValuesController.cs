58d26e23da0afe0a37f93f1b67798935f7408695
Michael Olan <olanmj@gmail.com>
2018-03-23 15:12:10 -0400

    Replace deleted Values controller

---
+++ Matricks/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MyMatrix.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {       
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = new string[] { "abc", "def", "ghi", "jkl" };
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            // throw new Exception("Test exception");
+            var value = "sample";
+            return Ok(value);
+        }
+
+    }
+}

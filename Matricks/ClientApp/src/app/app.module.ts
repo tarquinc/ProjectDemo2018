a3c9b537ec4ef2efdb427b60e690b3fafad1b550
Tarquinc <tarquinc@go.stockton.edu>
2018-04-11 12:45:57 -0400

    Added home and nav components

--- Matricks/ClientApp/src/app/app.module.ts
+++ Matricks/ClientApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
